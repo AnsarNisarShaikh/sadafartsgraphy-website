14954575daf7f98de820cc5fe52e7173a57f7bf5
AnsarAhmedDevelop <ansarahmedn@gmail.com>
2025-09-23 17:56:26 +0530

    small changes

--- next.config.mjs
+++ next.config.mjs
@@ -1,19 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
-  images: {
-    // qualities: [75, 90, 100], // <-- add the qualities you want to allow
-     domains: ["cdn.builder.io"],
-  },
+
   eslint: {
     ignoreDuringBuilds: true,
   },
   // typescript: {
   //   ignoreBuildErrors: true,
   // },
-  // images: {
-  //   unoptimized: true,
-  // },
+  images: {
+    unoptimized: true,
+  },
   // trailingSlash: true,
 };
 
