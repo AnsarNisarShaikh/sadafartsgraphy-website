a271e07e0ab6cdb2d8b83b9a6eabca0a93da3a6f
AnsarAhmedDevelop <ansarahmedn@gmail.com>
2025-10-03 16:47:04 +0530

    change sadaf image

--- components/home/HomeAbout.js
+++ components/home/HomeAbout.js
@@ -29,7 +29,7 @@ function HomeAbout() {
 
                     <div className="relative sm:hidden mt-5 sm:mt-0">
                         <img
-                            src="/images/about.png"
+                            src="/images/sadaf.jpg"
                             alt="Sadaf Khan"
 
 
@@ -61,7 +61,7 @@ function HomeAbout() {
                 <div className={`sm:block hidden `}>
                     <div className="relative">
                         <img
-                            src="/images/about.png"
+                            src="/images/sadaf.jpg"
                             alt="Sadaf Khan"
 
 
