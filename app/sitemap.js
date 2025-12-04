b6cf866b7984159d27e688da78a1ca4e2e400d82
AnsarNisarShaikh <ansarahmedn@gmail.com>
2025-12-04 13:23:38 +0530

    add sitemap code

---
+++ app/sitemap.js
@@ -0,0 +1,113 @@
+export default function sitemap() {
+  return [
+    {
+      url: 'https://sadafartsgraphy.com',
+      lastModified: new Date(),
+      changeFrequency: 'yearly',
+      priority: 1,
+    },
+    {
+      url: 'https://sadafartsgraphy.com/about',
+      lastModified: new Date(),
+      changeFrequency: 'monthly',
+      priority: 0.8,
+    },
+    {
+      url: 'https://sadafartsgraphy.com/my-work',
+      lastModified: new Date(),
+      changeFrequency: 'monthly',
+      priority: 0.8,
+    },
+    {
+      url: 'https://sadafartsgraphy.com/contact',
+      lastModified: new Date(),
+      changeFrequency: 'monthly',
+      priority: 0.8,
+    },
+    {
+      url: 'https://sadafartsgraphy.com/my-work/live-studio-calligraphy',
+      lastModified: new Date(),
+      changeFrequency: 'monthly',
+      priority: 0.8,
+    },
+    {
+      url: 'https://sadafartsgraphy.com/my-work/bespoke-calligraphy',
+      lastModified: new Date(),
+      changeFrequency: 'monthly',
+      priority: 0.8,
+    },
+    {
+      url: 'https://sadafartsgraphy.com/my-work/live-studio-engraving',
+      lastModified: new Date(),
+      changeFrequency: 'monthly',
+      priority: 0.8,
+    },
+    {
+      url: 'https://sadafartsgraphy.com/my-work/jewellery-illustration',
+      lastModified: new Date(),
+      changeFrequency: 'monthly',
+      priority: 0.8,
+    },
+    {
+      url: 'https://sadafartsgraphy.com/my-work/heat-foiling',
+      lastModified: new Date(),
+      changeFrequency: 'monthly',
+      priority: 0.8,
+    },
+    {
+      url: 'https://sadafartsgraphy.com/my-work/product-painting',
+      lastModified: new Date(),
+      changeFrequency: 'monthly',
+      priority: 0.8,
+    },
+    {
+      url: 'https://sadafartsgraphy.com/my-work/leather-painting',
+      lastModified: new Date(),
+      changeFrequency: 'monthly',
+      priority: 0.8,
+    },
+    {
+      url: 'https://sadafartsgraphy.com/my-work/mural-painting',
+      lastModified: new Date(),
+      changeFrequency: 'monthly',
+      priority: 0.8,
+    },
+    {
+      url: 'https://sadafartsgraphy.com/my-work/tote-bag-live',
+      lastModified: new Date(),
+      changeFrequency: 'monthly',
+      priority: 0.8,
+    },
+    {
+      url: 'https://sadafartsgraphy.com/my-work/artwork-customisation',
+      lastModified: new Date(),
+      changeFrequency: 'monthly',
+      priority: 0.8,
+    },
+    {
+      url: 'https://sadafartsgraphy.com/my-work/beads-making',
+      lastModified: new Date(),
+      changeFrequency: 'monthly',
+      priority: 0.8,
+    },
+    {
+      url: 'https://sadafartsgraphy.com/my-work/hand-embroidery-work',
+      lastModified: new Date(),
+      changeFrequency: 'monthly',
+      priority: 0.8,
+    },
+    {
+      url: 'https://sadafartsgraphy.com/my-work/silhouette-arts',
+      lastModified: new Date(),
+      changeFrequency: 'monthly',
+      priority: 0.8,
+    },
+    {
+      url: 'https://sadafartsgraphy.com/my-work/origami-work',
+      lastModified: new Date(),
+      changeFrequency: 'monthly',
+      priority: 0.8,
+    },
+  
+  ]
+}
